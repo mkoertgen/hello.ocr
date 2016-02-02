07dece8502f11f183f407fc08e4d8cb6308fd7b0
mkoertgen <marcel.koertgen@gmail.com>
2016-02-02 16:42:19 +0100

    Initial commit

---
+++ hello.ocr/OcrLearningTests.cs
@@ -0,0 +1,29 @@
+﻿//using Windows.ApplicationModel;
+//using Windows.Globalization;
+//using Windows.Graphics.Imaging;
+//using Windows.UI.Xaml.Controls;
+//using Windows.Media.Ocr;
+using NUnit.Framework;
+
+namespace hello.ocr
+{
+    /// <summary>
+    /// http://www.hanselman.com/blog/HowToCallWinRTAPIsInWindows8FromCDesktopApplicationsWinRTDiagram.aspx
+    /// https://software.intel.com/en-us/articles/using-winrt-apis-from-desktop-applications
+    /// </summary>
+    [TestFixture]
+    internal class OcrLearningTests
+    {
+        //OcrEngine ocrEngine;
+
+        public OcrLearningTests()
+        {
+            //ocrEngine = OcrEngine.TryCreateFromLanguage(new Language("en"));
+        }
+
+        [Test]
+        public void TestOcr()
+        {
+        }
+    }
+}
